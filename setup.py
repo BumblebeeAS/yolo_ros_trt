885de503350cace8f1b80b5fb6941a24876b8fb4
SamuelFoo <fooenzesamuel@gmail.com>
2025-05-28 04:03:19 +0800

    Move launch file to vision_pipeline

--- setup.py
+++ setup.py
@@ -20,14 +20,6 @@ setup(
             os.path.join("share", package_name, "src"),
             glob(os.path.join("src", "*.py")),
         ),
-        (
-            os.path.join("share", package_name, "launch"),
-            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
-        ),
-        (
-            os.path.join("share", package_name, "config"),
-            glob(os.path.join("config", "*.yaml")),
-        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
