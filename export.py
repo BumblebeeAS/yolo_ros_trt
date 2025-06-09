9f073169b6c848db77039f490899056345f6c8f6
SamuelFoo <fooenzesamuel@gmail.com>
2025-06-09 23:14:48 +0800

    Use model file path instead for export.py

--- export.py
+++ export.py
@@ -2,13 +2,13 @@ import typer
 from ultralytics import YOLO
 
 
-def export_model_to_engine(model_file_name: str) -> None:
+def export_model_to_engine(model_file_path: str) -> None:
     """Export the YOLO model to TensorRT engine format.
 
     Args:
-        model_file_name (str): The name of the model file.
+        model_file_path (str): The path of the model file.
     """
-    model = YOLO(f"models/{model_file_name}")
+    model = YOLO(model_file_path)
     model.export(format="engine")
 
 
